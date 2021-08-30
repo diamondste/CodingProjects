0f773fe5ef357b85c587a2ea17171ff5c054a35e
Diamond Stephens <diamond.stephens2@gmail.com>
2021-08-29 20:23:45 -0400

    Create App.js

---
+++ MERN/Hook_Form/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from './components/Forms';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
