523a5d1fb8373e41aa37dda71818e6d1ca14edee
Diamond Stephens <diamond.stephens2@gmail.com>
2021-09-07 15:34:25 -0400

    Create App.js

---
+++ MERN/ProductManager/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './views/Main'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Manager</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
