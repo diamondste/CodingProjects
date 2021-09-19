685284e1695d1a8b7b71b44f86d1c55ebbc50f38
Diamond Stephens <diamond.stephens2@gmail.com>
2021-09-18 23:03:32 -0400

    Update author.routes.js

--- MERN/Authors/server/routes/author.routes.js
+++ MERN/Authors/server/routes/author.routes.js
@@ -1,4 +1,8 @@
-const AuthorController = require('../controller/author.controller');
+const AuthorController = require('../controllers/author.controller');
 module.exports = function(app){
-    app.post('/api/author', AuthorController.createAuthor);
+    app.get('/api/authors', AuthorController.getAllAuthors);
+    app.post('/api/authors', AuthorController.createAuthor);
+    app.get('/api/authors/:id', AuthorController.getAuthor);
+    app.put('/api/authors/:id', AuthorController.updateAuthor);
+    app.delete('/api/authors/:id', AuthorController.deleteAuthor)
 }
