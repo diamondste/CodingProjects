24292a86a7dc95071633a1d59357b95b98e34d78
Diamond Stephens <diamond.stephens2@gmail.com>
2021-09-18 23:04:30 -0400

    Update author.controller.js

--- MERN/Authors/server/controller/author.controller.js
+++ MERN/Authors/server/controller/author.controller.js
@@ -1,9 +1,35 @@
 const Author = require('../models/author.model');
-module.exports.createAuthor = (request, response) => {
-    const {authorName} = request.body;
-    Author.create({
-        authorName
-    })
-        .then(author => response.json(author))
-        .catch(err => response.json(err));
+
+module.exports.getAllAuthors = (request, response) => {
+    Author.find({})
+        .then(authors => response.json(authors))
+        .catch(err => response.json(err))
 }
+
+module.exports.getAuthor = (request, response) => {
+    Author.findOne({_id:request.params.id})
+        .then(author => response.json(author))
+        .catch(err => response.json(err))
+}
+module.exports.createAuthor =  (request, response) => {
+        const {name} = request.body;
+        Author.create({
+            name: name
+        })
+            .then(newAuthor => response.json(newAuthor))
+            .catch(err => response.status(400).json(err));
+}
+
+module.exports.updateAuthor = (request, response) => {
+    Author.findOneAndUpdate({_id: request.params.id}, request.body,
+{new:true})
+    .then(editAuthor => response.json(editAuthor))
+    .catch(err => response.json(err))
+}
+
+module.exports.deleteAuthor = (request, response) => {
+    Author.deleteOne({_id: request.params.id})
+    .then(deleteConfrim => response.json(deleteConfrim))
+    .catch(err => response.json(err))
+}
+
